aa5de9ab14f0f460aa9dd3d602862d638e0f4ebb
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2022-04-19 21:26:39 +0200

    first working version of b-splines

--- Project04-BSplinesPoisson/bsplinespoisson.cpp
+++ Project04-BSplinesPoisson/bsplinespoisson.cpp
@@ -10,13 +10,26 @@ using namespace Eigen;
  * @brief Returns the non-zero B-splines at x.
  *
  * @param x The position to evaluate the splines at
- * @param knots The knots defining the splines. (without ghost points!)
+ * @param knots Increasing sequence of knots defining the splines. (without
+ * ghost points!)
  * @param order The order of the splines this determines the length of the
  * output. indexing convention: lowest order is order=0
- * @return SparseVector<double> with `order+1` nonzero elements
+ * @return {VectorXd spline_values, idx} the spline values and the index
  */
-SparseVector<double> bsplines(double x, const VectorXd& knots, Index order) {
+std::tuple<VectorXd, Index> bsplines(double x, const ArrayXd& knots,
+                                     Index order) {
   // TODO: Boundary check on x
+
+  auto n_knots = knots.size();
+
+  auto get_knot = [&knots, &n_knots](int i) {
+    if (i < 0)
+      return knots[0];
+    else if (n_knots <= i)
+      return knots[n_knots - 1];
+    else
+      return knots[i];
+  };
 
   // Find right index
   Index idx = 0;
@@ -30,38 +43,37 @@ SparseVector<double> bsplines(double x, const VectorXd& knots, Index order) {
   iter(order) = 1;
 
   for (Index cur_order = 1; cur_order <= order; cur_order++) {
-    for (Index i = order - cur_order; i < order; i++) {
-      const Index knots_idx = idx + i - order;
+    for (Index i = idx - cur_order; i < idx; i++) {
+      const Index iter_idx = i - idx + order;
+      double w1 = 0, w2 = 0;
+      // check if both ghost points
+      if (i < n_knots && i + cur_order > 0)
+        w1 = (x - get_knot(i)) / (get_knot(i + cur_order) - get_knot(i));
+      if (i + 1 < n_knots)
+        w2 = (get_knot(i + cur_order + 1) - x) /
+             (get_knot(i + cur_order + 1) - get_knot(i + 1));
 
-      // check if ghost point
-      if (knots_idx >= 0 && knots_idx + cur_order < knots.size()) {
-        double w1 = (x - knots[knots_idx]) /
-                    (knots[knots_idx + cur_order] - knots[knots_idx]);
-        iter(i) = w1 * iter(i);
-
-        if (knots_idx + cur_order + 1 < knots.size()) {
-          double w2 = (knots[knots_idx + cur_order + 1] - x) /
-                      (knots[knots_idx + cur_order + 1] - knots[knots_idx + 1]);
-          iter(i) += w2 * iter(i + 1);
-        }
-      }
+      iter(iter_idx) = w1 * iter(iter_idx) + w2 * iter(iter_idx + 1);
     }
     // this replaces the left 'ghost points'
-    double w1 = (x - knots[idx]) / (knots[idx + cur_order] - knots[idx]);
+    double w1 = 0;
+    if (idx < knots.size())
+      w1 = (x - get_knot(idx)) / (get_knot(idx + cur_order) - get_knot(idx));
+
     iter(order) = w1 * iter(order);
   }
 
-  SparseVector<double> return_vector(knots.size());
-  return_vector.reserve(order + 1);
-  for (Index i = idx - order; i < idx; i++)
-    return_vector.insert(i) = iter(i - idx + order);
-
-  return return_vector;
+  return {iter, idx};
 }
 
 int main(int argc, char const* argv[]) {
-  Eigen::ArrayXd array;
-  std::string name;
-
+  using std::cout, std::endl;
+  ArrayXd knots = ArrayXd::LinSpaced(11, 0, 1);
+  cout << "The knots are: " << knots.transpose() << endl;
+  double x = .95;
+  Index order = 3;
+  auto [values, index] = bsplines(x, knots, order);
+  cout << "The spline value at " << x << " is: " << values.transpose() << endl;
+  cout << "Summing all spline values (should be 1): " << values.sum() << endl;
   return 0;
 }
