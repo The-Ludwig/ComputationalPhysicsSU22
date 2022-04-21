b77cacd6d79e984a25c7b8cb0bbdd58d9260f3fc
Ludwig Neste <ludwig.neste@tu-dortmund.de>
2022-04-21 15:18:19 +0200

    :tada: Nice BSpline implementation

--- Project04-BSplinesPoisson/test.cpp
+++ Project04-BSplinesPoisson/test.cpp
@@ -7,13 +7,13 @@ int main() {
   int order = 3;
   std::vector<double> xs = {0, .5, 1};
 
-  for (double x : xs) {
-    auto [spline_values, index] =
-        bspline::bsplines(x, knots, std::size_t(order));
-    for (int i = 0; i <= order; i++)
-      std::cout << "Spline with index " << int(index) - int(order - i) << " at "
-                << x << " =" << spline_values[i] << std::endl;
-  }
+  double x = .5;
+  auto [spline_values, index] =
+      bspline::ndx_bsplines(x, knots, std::size_t(order), 2);
+  for (int i = 0; i <= order; i++)
+    std::cout << "Spline second derivative with index "
+              << int(index) - int(order - i) << " at " << x << " ="
+              << spline_values[i] << std::endl;
   // Expected output:
   // Spline with index 1 (0.5) =0
   // Spline with index 2 (0.5) =0.166667
